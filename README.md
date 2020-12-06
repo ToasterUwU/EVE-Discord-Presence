8dc52abbdb74c9ff77ab4bfa7f93a4c228aa42f0
ToasterUwU <marvinator11@gmail.com>
2020-12-07 00:43:52 +0100

    1.0

--- README.md
+++ README.md
@@ -6,7 +6,15 @@ A application that enables Rich Presence for EVE Online.
 
 ## Usage
 
-1. Download newest release
-2. Start the game and log in with a character
-3. Start this Software
-4. Close the Software after you stopped playing
+1. Download the Setup of newest release: [Click me](https://github.com/ToasterUwU/EVE-Discord-Presence/releases/latest)
+2. Use the Setup to install the program
+3. Start EVE Online and select a character
+4. Start EVE Discord Presence
+5. Click the "Start Presence" button
+
+## To Do
+
+- Save Settings
+- Setting for autostarting the Presence
+- Option to select specific character (currently uses the one that was last logged in)
+- Better UI (No Clue how by now)
--- main.py
+++ main.py
@@ -2,11 +2,27 @@ import os
 import time
 import pypresence
 import getpass
+import tkinter
+import tkinter.ttk as ttkinter
+import threading
 
 client_id = "785146044056076328" # Discord Application Client ID. Replace with your own, if you dont use the official build.
 
 presence = pypresence.Presence(client_id)
 presence.connect()
+
+tk = tkinter.Tk("EVE Online - Discord Presence")
+tk.title("EVE Online - Discord Presence")
+try:
+    tk.iconbitmap("icon.ico")
+except:
+    pass
+tk.geometry("350x100")
+
+frame = tkinter.Frame()
+
+run_th = False
+show_location = tkinter.BooleanVar(frame, value=True)
 
 def log_lines():
     """
@@ -27,16 +43,15 @@ def details(lines=None):
         lines = log_lines()
 
     char = lines[2].split("Listener: ")[1]
-    details = {"char": char, "location": "Unknown", "autopilot": False, "docked": "Docked in "}
+    details = {"char": char, "location": "Unknown", "autopilot": False, "docked": True}
 
     for line in lines:
-        if "(None)" in line:
-            if "Undocking from" in line:
-                details["location"] = line.split(" to ")[1].replace(" solar system.", "")
-                details["docked"] = "In Space of "
+        if "Undocking from" in line:
+            details["location"] = line.split(" to ")[1].replace(" solar system.", "")
+            details["docked"] = False
 
-            elif "Jumping from" in line:
-                details["location"] = line.split(" to ")[1]
+        elif "Jumping from" in line:
+            details["location"] = line.split(" to ")[1]
 
         elif "Autopilot engaged" in line:
             details["autopilot"] = True
@@ -45,18 +60,59 @@ def details(lines=None):
             details["autopilot"] = False
 
         elif "Your docking request has been accepted." in line:
-            details["docked"] = "Docked in "
+            details["docked"] = True
     return details
 
-while True:
-    d = details()
+def loop_thread():
+    global run_th
+    while run_th:
+        d = details()
 
-    if d["autopilot"] == False:
-        small_img = "manual"
-        small_txt = "Manual flight"
+        if d["autopilot"] == False:
+            if d["docked"]:
+                small_img = "docked"
+                small_txt = "Docked"
+            else:
+                small_img = "manual"
+                small_txt = "Manual flight"
+        else:
+            small_img = "auto"
+            small_txt = "Autopilot"
+
+        if not show_location.get():
+            d["location"] = ""
+
+            if d["docked"]:
+                d["docked"] = "Docked"
+            else:
+                d["docked"] = "In Space"
+        else:
+            if d["docked"]:
+                d["docked"] = "Docked in "
+            else:
+                d["docked"] = "In Space of "
+
+        presence.update(state=d["docked"]+d["location"], details=d['char'], large_image="eve-logo", large_text="EVE Online", small_image=small_img, small_text=small_txt)
+        time.sleep(5)
+
+def change_th():
+    global th, run_th, th_button
+    if run_th == False:
+        th = threading.Thread(target=loop_thread, daemon=True)
+        run_th = True
+        th.start()
+        th_button["text"] = "Stop Presence"
     else:
-        small_img = "auto"
-        small_txt = "Autopilot"
+        run_th = False
+        th_button["text"] = "Start Presence"
+        presence.clear()
 
-    presence.update(state=d["docked"]+d["location"], details=d['char'], large_image="eve-logo", large_text="EVE Online", small_image=small_img, small_text=small_txt)
-    time.sleep(5)
+th_button = ttkinter.Button(frame, text="Start Presence", command=change_th)
+th_button.pack()
+
+location_box = ttkinter.Checkbutton(frame, text="Show Location", variable=show_location, onvalue=True, offvalue=False)
+location_box.pack()
+
+frame.place(relx=0.5, rely=0.5, anchor="c")
+
+tk.mainloop()
